f267e6b9e2493ae9dee0abe97f9b2381a6eac019
Fabian Haglund <till.fabian@gmail.com>
2020-04-02 16:18:46 +0200

    EventItem

---
+++ src/components/EventItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class EventItem extends React.Component {
+
+    
+
+    render(){
+        return(
+            <div>EventItem</div>
+        );
+    }
+
+}
+
+export default EventItem;
