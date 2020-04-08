78a61934c51ef1bfeeeab1a64b650eb175d88823
vladislav <vladik@live.se>
2020-04-08 20:05:30 +0200

    Har börjat med node server, lagt till axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 
 import NavBar from './components/NavBar.js';
@@ -7,15 +8,34 @@ import EventItemListView from './components/EventItemListView.js';
 import EventItem from './components/EventItem.js';
 import TextInput from './components/TextInput.js';
 
-
 class App extends Component {
+    constructor(props){
+	super(props);
+	this.state= {
+	    name: 'hejsan',
+	    data1: "boll",
+	};
 
-  render(){
-    return (
-      <div className="App" style={{ fontFamily: 'Inter' }}>
-        <NavBar/>
-        <EventItemListView />
-      </div>
+    }
+    vidKnappTryck = () => {
+	console.log("Funkar123");
+	axios.post("/",{
+	    data1: "hejsan123",
+	}).then((response)=> {
+	    console.log("Data submitted successfully");
+	}).catch((error)=> {
+	    console.log("got errr while posting data", error);	    
+	});
+    }
+		 
+    render(){
+	return (
+	    <div className="App" style={{ fontFamily: 'Inter' }}>
+            <NavBar/>
+            <EventItemListView />
+	    <button onClick={this.vidKnappTryck} id="loginKnapp" type="button" className="input">Server-TestKnapp</button>
+	    </div>
+	    
     ); 
   }
 }
