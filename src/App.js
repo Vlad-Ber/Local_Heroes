2e745d806c965e5ae8a8d96c1b3361e3a4aa0821
Fabian Haglund <till.fabian@gmail.com>
2020-04-08 11:16:24 +0200

    turned App into class component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar.js';
 import EventItemListView from './components/EventItemListView.js';
 
-function App() {
-  return (
-    <div className="App" style={{ fontFamily: 'Inter' }}>
-      <NavBar/>
-      <EventItemListView />
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App" style={{ fontFamily: 'Inter' }}>
+        <NavBar/>
+        <EventItemListView />
+      </div>
+    ); 
+  }
 }
 
 export default App;
