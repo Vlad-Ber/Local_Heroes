df80667dc1ef44ff269b96ead6913749ca6ea3ca
daba6626 <danno-082011@hotmail.com>
2020-04-08 13:50:39 +0200

    Added ConfirmButton Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 
 import EventItem from './components/EventItem.js';
-
+import ConfirmButton from './components/ConfirmButton.js';
 function App() {
   return (
     <div className="App">
       Hello FESS
-      <EventItem/>
+          <EventItem/>
+          <ConfirmButton/>
+      
     </div>
   );
 }
---
+++ src/components/ConfirmButton.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ConfirmButton extends React.Component {
+
+
+    render(){
+        return(
+                <div>
+                <button>Confirm!</button>
+                </div>
+        )
+        
+    }
+}
+
+export default ConfirmButton;
