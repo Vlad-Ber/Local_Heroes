bc4eab3ff8a6b37788ae9f70a46aff7878082e11
Fabian Haglund <till.fabian@gmail.com>
2020-04-09 13:15:07 +0200

    fix intendation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-
 import NavBar from './components/NavBar.js';
 import EventItemListView from './components/EventItemListView.js';
 import SectionTitle from './components/SectionTitle.js';
@@ -23,14 +22,14 @@ class App extends Component {
   }
 
   vidKnappTryck = () => {
-	axios.post("/",{
-	    data1: "hej",
-	}).then((response)=> {
-	    console.log("Data submitted successfully");
-	}).catch((error)=> {
-	    console.log("got errr while posting data", error);	    
-	});
-    }
+    axios.post("/",{
+        data1: "hej",
+    }).then((response) => {
+        console.log("Data submitted successfully");
+    }).catch((error) => {
+        console.log("got errr while posting data", error);	    
+    });
+  }
 
   componentDidMount(){
     let updatedActiveUsers = data["users"].filter(user => 
