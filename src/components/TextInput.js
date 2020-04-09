707b9c97466bf2cd1d0868d40fbc1069b88603f0
Jonathan <jonathannoahtadese@gmail.com>
2020-04-09 12:49:00 +0200

    improved styling of text input

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
+
+
+const Input = styled.input`
+    border:  1px solid #31D285;
+    box-sizing: border-box;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.12);
+
+    height: 30px;
+    width: 14em;
+`
+
 class TextInput extends React.Component {
-  state = {
-    value: '',
-  }
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        value: '',
+      }
+    }
 
   saveText = (event) => {
    this.setState({value: event.target.value});
@@ -13,12 +30,12 @@ class TextInput extends React.Component {
   render() {
     console.log(this.state.value)
     return (
-      <form>
-        <input type="text" value={this.state.value} onChange={this.saveText} />
-        <input type="submit" value="Submit" />
-      </form>
+        <Input type="text" value={this.state.value} onChange={this.saveText}/>
     );
   }
 }
+
+
+
 
 export default TextInput;
