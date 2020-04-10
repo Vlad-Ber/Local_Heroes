79ee6aa3b4d1bdd7ba78fc7559524d5b1c6b0b2b
daba6626 <danno-082011@hotmail.com>
2020-04-10 11:12:44 +0200

    tested confirmbutton

--- src/components/ConfirmButton.js
+++ src/components/ConfirmButton.js
@@ -4,14 +4,17 @@ import {Checkmark} from 'react-checkmark';
 
 import './../confirmButton.css';
 class ConfirmButton extends React.Component {
-
-  
+    
+    print(){
+        
+        console.log('hej');
+    }
     render(){
         return(
                 <div>
                 
-                
-                <button id="confirm" >
+            
+                <button id="confirm" onClick={this.print}>
                 <Checkmark size='large'/>
                 </button>
                 </div>
