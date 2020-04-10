bbfe8ebd32ef6ad651d254a1901ee1e7f6a78a6b
Jonathan <jonathannoahtadese@gmail.com>
2020-04-10 14:26:55 +0200

    made Zip Code layout

---
+++ src/pages/ZipCode.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+import SectionTitle from '../components/SectionTitle.js';
+import TextInput from '../components/TextInput.js'
+
+class ZipCode extends React.Component {
+
+  render(){
+    return (
+      <div>
+        <TitleWrapper>
+          Welcome to LocalHero!
+        </TitleWrapper>
+
+        <SectionTitle
+          text = "Please enter your zip code"
+          titleType = "variant"
+        />
+        <TextInput />
+      </div>
+    );
+  }
+}
+
+const TitleWrapper = styled.div`
+    font-size: 24px;
+    text-align: center;
+    padding-top: 70px;
+    padding-bottom: 40px;
+    font-weight: bold;
+`
+
+export default ZipCode;
