74d1da25b2476f72a5fb9544c201c94be8af4b4f
Fabian Haglund <till.fabian@gmail.com>
2020-04-10 13:25:20 +0200

    started working on HelpRequest page

---
+++ src/pages/HelpRequest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; 
+
+class HelpRequest extends Component {
+    render(){
+        return(
+            <div>HelpRequest</div>
+        );
+    }
+}
+
+export default HelpRequest;
