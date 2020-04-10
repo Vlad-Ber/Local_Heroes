717a99a9bbd2ed62219ca71570563d72af4d2c34
vladislav <vladik@live.se>
2020-04-10 15:09:46 +0200

    Skrev en generell find funktion för mongo

--- server.js
+++ server.js
@@ -11,11 +11,45 @@ const client = new MongoClient(uri, {
     useUnifiedTopology: true,
     useNewUrlParser: true });
 client.connect(err => {
+//----------------------------MONGODB FUNKTIONER-------------------------------------------------------------
     const db = client.db("LocalHeroes");
     const areas = db.collection("Areas");
-    // perform actions on the collection object
+    const errands = db.collection("Errands");
+    const users = db.collection("Users");
 
+    async function document_exist(collection, document_query){
+	const coll = db.collection(collection);
+	var foundQuery = await coll.findOne(document_query);
+	if (foundQuery == null){
+	    return false;
+	}
+	else{
+	    return true;
+	}
+    }
+    
+    async function insertUser(email, name, age, adress, description,areaID){
+	var data = {
+	    "email": email,
+	    "name": name,
+	    "age": age,
+	    "adress": adress,
+	    "desription": description,
+	    "virtuePoints": 0,
+	    "areaID": areaID,
+	};
+	var queryToFind = {"email": email};
+	var findUser = await document_exist("Users", queryToFind);
+	if( findUser == false){
+	    users.insertOne(data).catch(error =>console.error(error));
+	    console.log("User " + name + " has been added!");
+	}
+	else{
+	    console.log("A user with this email already exists");
+	}
+    };
 
+//-----------------------------------------------------------------------------------------------------------
 
     //app.use(bodyParser.urlencoded({ extended: false })); : DETTA KANSKE BEHÖVS I FRAMTIDEN
     app.use(bodyParser.json());
@@ -23,7 +57,7 @@ client.connect(err => {
     var router = express.Router();
 
     // console.log that your server is up and running
-
+    
     
     // create a GET route
     app.get('/api/greeting', (req, res) => {
@@ -33,8 +67,7 @@ client.connect(err => {
     app.post('/', function(req, res) {
 	var testData = req.body.data1;
 	var dataToSend = {"testData1":testData, "testdata2": "boll"}
-	console.log(testData);
-	areas.insertOne(dataToSend).catch(error =>console.error(error));
+	insertUser("olle@hotmail.com", "Olle Eriksson", 20, "Sveavägen 1", "Gillar att laga mat", 75757);
     });
 });
 client.close();
