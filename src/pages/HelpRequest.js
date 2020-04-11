c702230a140d4a4fc9fd49d950b2132d0acd43b5
Fabian Haglund <till.fabian@gmail.com>
2020-04-11 02:05:01 +0200

    added generic DropDown component

--- src/pages/HelpRequest.js
+++ src/pages/HelpRequest.js
@@ -5,13 +5,31 @@ import styled from 'styled-components';
 import NavBar from '../components/NavBar.js';
 import SectionTitle from '../components/SectionTitle.js';
 import TextInput from '../components/TextInput.js';
+import DropDownInput from '../components/DropDownInput.js';
 
 class HelpRequest extends Component {
+
     render(){
+
+        let options = [
+            {
+                text: "Carrying",
+                value: "carrying", 
+            }, 
+            {
+                text: "Shopping",
+                value: "shopping"
+            }, 
+        ]
+
         return(
             <div>
                 <NavBar/>
                 <SectionTitle text="HELP REQUEST"/>
+
+                <InputWrapper>
+                    <DropDownInput options={options}/>
+                </InputWrapper>
 
                 <DescriptionInput>
                     <SectionTitle fontSize="14px" text="What do you need help with?"/>
