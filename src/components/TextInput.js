8a55f0aa28ea6b675125d8a5c621c1ba4c8c46b3
Fabian Haglund <till.fabian@gmail.com>
2020-04-15 22:54:06 +0200

    Merged duplicate implementations of TextInput, rm DropDown type

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
+
 import styled from 'styled-components'
 
-
-class TextInput extends React.Component {
+class TextInput extends Component {
 
   constructor(props) {
       super(props);
@@ -16,9 +16,34 @@ class TextInput extends React.Component {
     this.setState({value: event.target.value});
   }
 
-  render() {
-<<<<<<< HEAD
-    return (
+  renderInputType = () => {
+    let type = this.props.type; 
+    switch(type){
+      case "number": 
+        return (
+        <StyledInput 
+          type="number" 
+          value={this.state.value} 
+          onChange={this.saveInput} 
+          height={this.props.height} 
+          width={this.props.width} 
+          min="0"
+        />
+        );
+
+      case "password": 
+        return (
+        <StyledInput 
+          type="password" 
+          value={this.state.value} 
+          onChange={this.saveInput}
+          height={this.props.height} 
+          width={this.props.width} 
+        />
+        );
+
+      default: 
+        return (
         <StyledInput 
           type="text" 
           value={this.state.value} 
@@ -26,32 +51,12 @@ class TextInput extends React.Component {
           height={this.props.height} 
           width={this.props.width} 
         />
-    );
-=======
-    let type = this.props.type;
-    let inputType;
-
-    switch(type){
-        case "dropdown":
-            inputType = <StyledDropdown>
-                          <option value = "Male">MALE</option>
-                          <option value = "Female">FEMALE</option>
-                          <option value = "Other" selected>OTHER</option>
-                        </StyledDropdown>
-            break;
-        case "number":
-            inputType = <StyledInput type = "number" value={this.state.value} onChange={this.saveInput} min = "0"/>
-            break;
-        case "password":
-            inputType = <StyledInput type = "password" value={this.state.value} onChange={this.saveInput}/>
-            break;
-        default:
-            inputType = <StyledInput type = "text" value={this.state.value} onChange={this.saveInput}/>
-            break;
+        );
     }
+  }
 
-    return inputType;
->>>>>>> master
+  render(){
+    return({renderInputType()})
   }
 }
 
@@ -65,22 +70,6 @@ const StyledInput = styled.textarea`
 
     height: ${props => props.height || '24em'};
     width: ${props => props.width || '18em'};
-`
-
-const StyledDropdown = styled.select`
-    margin: auto;
-    display: flex;
-
-    border:  1px solid #31D285;
-    box-sizing: border-box;
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.12);
-    background: #FFFFFF;
-
-    height: 30px;
-    width: 18em;
-
-    opacity: 0.8;
-    text-align: center;
 `
 
 export default TextInput;
