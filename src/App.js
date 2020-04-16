563cd2cf049e8a88a0e3d87962524a94ccd990aa
Jonathan <jonathannoahtadese@gmail.com>
2020-04-16 11:49:46 +0200

    made layout for area code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home.js'; 
+import Home from './pages/Home.js';
 import HelpRequest from './pages/HelpRequest.js';
 import Signup from './pages/Signup.js';
+import ZipCode from './pages/ZipCode.js'
+import ProfileCreation from './pages/ProfileCreation.js'
 
 class App extends Component {
 
@@ -15,6 +17,8 @@ class App extends Component {
             <Route path="/" exact component={Home}/>
             <Route path="/help-request" component={HelpRequest}/>
             <Route path="/signup" component={Signup}/>
+            <Route path="/zipcode" component={ZipCode}/>
+            <Route path="/profile-creation" component={ProfileCreation}/>
           </Switch>
         </div>
       </Router>
