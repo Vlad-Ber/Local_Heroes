709ddf852e04deaffe8e6a7965e87966349da34c
Jonathan <jonathannoahtadese@gmail.com>
2020-04-17 16:59:25 +0200

    fixing rebase

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ class App extends Component {
 
             <Route path="/zipcode" component={ZipCode}/>
             <Route path="/profile-creation" component={ProfileCreation}/>
-
           </Switch>
         </div>
       </Router>
