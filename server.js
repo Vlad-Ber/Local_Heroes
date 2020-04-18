b9a29b6a3f30d72a19cb67fc6eb819aaa6522911
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2020-04-18 15:18:25 +0200

    Working on

--- server.js
+++ server.js
@@ -20,98 +20,108 @@ client.connect(err => {
 
     //Generell function to check if a document exist
     async function documentExist(collection, document_query){
-	const coll = db.collection(collection);
-	let foundQuery = await coll.findOne(document_query);
-	if (foundQuery == null){
+	      const coll = db.collection(collection);
+	      let foundQuery = await coll.findOne(document_query);
+	      if (foundQuery == null){
 	          return false;
-	}
-	else{
-	    return true;
-	}
+	      }
+	      else{
+	          return true;
+	      }
     }
 
     async function insertArea(areaID, email){
-	var data = {
-	    "areaID": areaID,
-	    "users": [email],
-	    "errands": [],
-	};
-	await areas.insertOne(data).catch(error =>console.error(error));
-	console.log("Area with ID " + areaID + " has been added!");
+	      var data = {
+	          "areaID": areaID,
+	          "users": [email],
+	          "errands": [],
+	      };
+	      await areas.insertOne(data).catch(error =>console.error(error));
+	      console.log("Area with ID " + areaID + " has been added!");
     }
 
     async function updateArea(areaID, email){
-	var areaToFind = {"areaID": areaID};
-	areas.updateOne(areaToFind, {"$push": {"users": email } } )
+	      var areaToFind = {"areaID": areaID};
+	      areas.updateOne(areaToFind, {"$push": {"users": email } } )
     }
     
     async function getErrandsArea(areaID){
-	errands.find({"areaID": areaID}).toArray(function(err, result) {
-	    if (err) throw err;
-	    return result;
-	});
+	      await errands.find({"areaID": areaID}).toArray(function(err, result) {
+	          if (err) throw err;
+	          return result;
+	      });
     }
     
     async function getErrandsEmail(email){
-	      errands.find({})
-    }
+	      await errands.find({"requester": email }).toArray(function(err, result){
+            if(err) throw err;
+            if(result.length == 0){
+                //TODO: Change this to send information to frontend
+                console.log("Current user does not have any errands");
+            }
+            //TODO: Delete the else here
+            else{
+                return result;
+            }
+        });
+    };
 
     
     async function insertUser(email, name, age, adress, description,areaID){
-	var data = {
-	    "email": email,
-	    "name": name,
-	    "age": age,
-	    "adress": adress,
-	    "desription": description,
-	    "virtuePoints": 0,
-	    "areaID": areaID,
-	};
-	var queryToFind = {"email": email};
-	var findUser = await documentExist("Users", queryToFind);
-	if( findUser == false){
-	    await users.insertOne(data).catch(error =>console.error(error));
-	    console.log("User " + name + " has been added!");
-	    var areaToFind = {"areaID": areaID};
-	    var findArea = await documentExist("Areas", areaToFind);
-	    if (findArea == false){
-		await insertArea(areaID, email);
-	    }
-	    else{
-		await updateArea(areaID, email);
-	    }
-	}
-	else{
-	    console.log("A user with this email already exists");
-	}
-	
+	      var data = {
+	          "email": email,
+	          "name": name,
+	          "age": age,
+	          "adress": adress,
+	          "desription": description,
+	          "virtuePoints": 0,
+	          "areaID": areaID,
+	      };
+	      var queryToFind = {"email": email};
+	      var findUser = await documentExist("Users", queryToFind);
+	      if( findUser == false){
+	          await users.insertOne(data).catch(error =>console.error(error));
+	          console.log("User " + name + " has been added!");
+	          var areaToFind = {"areaID": areaID};
+	          var findArea = await documentExist("Areas", areaToFind);
+	          if (findArea == false){
+		            await insertArea(areaID, email);
+	          }
+	          else{
+		            await updateArea(areaID, email);
+	          }
+	      }
+	      else{
+	          console.log("A user with this email already exists");
+	      }
+	      
     };
 
 
-   
+    
     
     async function insertErrand(title, description, requester,  type, adress, contact, areaID){
-	var date = new Date();
-	var dateString= date.toISOString().slice(0,10);
-	var data = {
-	    "createdAt": dateString, //Future improvement, show hours ago created
-	    "closedAt": "",
-	    "status": "Waiting",
-	    "title": title,
-	    "description": description,
-	    "adress": adress,
-	    "contact": contact,
-	    "helper": "",
-	    "requester": requester,    //TODO: koppla requester till userID
-	    "areaID": areaID,
-	};
-	var insert = await errands.insertOne(data).catch(error =>console.error(error));
-	var insertedId = insert.insertedId;
-	var areaToUpdate = {"areaID": areaID};
-	areas.updateOne(areaToUpdate, {"$push": {"errands": insertedId } } )
-	
+	      var date = new Date();
+	      var dateString= date.toISOString().slice(0,10);
+	      var data = {
+	          "createdAt": dateString, //Future improvement, show hours ago created
+	          "closedAt": "",
+	          "status": "Waiting",
+	          "title": title,
+	          "description": description,
+	          "adress": adress,
+	          "contact": contact,
+	          "helper": "",
+	          "requester": requester,    //TODO: koppla requester till userID
+	          "areaID": areaID,
+	      };
+	      var insert = await errands.insertOne(data).catch(error =>console.error(error));
+	      var insertedId = insert.insertedId;
+	      var areaToUpdate = {"areaID": areaID};
+	      areas.updateOne(areaToUpdate, {"$push": {"errands": insertedId } } )
+	      
 
-	console.log("Errand has been created by " + requester);
+	      console.log("Errand has been created by " + requester);
     }
     
 
@@ -145,18 +155,24 @@ client.connect(err => {
     });
 
     app.post('/', function(req, res) {
-	var testData = req.body.data1;
-	var dataToSend = {"testData1":testData, "testdata2": "boll"}
-	//insertUser("markus@gmail.com", "Markus Ollesson", 20, "Kungsvägen 1", "Lyfter tungt", 75565);
-	//insertUser("olle@gmail.com", "Olle Ollesson", 20, "Sveavägen 1", "Lagar mat", 75757);
-	//insertErrand("Laga mat", "Handla mjölk på Ica", "Anna", "Shopping", "Ringvägen 2", "07567467", 56788);
-	getErrandsArea(75757);
+	      var testData = req.body.data1;
+	      var dataToSend = {"testData1":testData, "testdata2": "boll"}
+	      //insertUser("markus@gmail.com", "Markus Ollesson", 20, "Kungsvägen 1", "Lyfter tungt", 75565);
+	      //insertUser("olle@gmail.com", "Olle Ollesson", 20, "Sveavägen 1", "Lagar mat", 75757);
+	      //insertErrand("Laga mat", "Handla mjölk på Ica", "Anna", "Shopping", "Ringvägen 2", "07567467", 56788);
+        
+	      //getErrandsArea(75757);
+        let test123 = await getErrandsEmail("anna@gmail.com");
+        console.log(123);
+       
+       
+        
     });
 
-    app.post('getErrands', function(req, res) {
-	var errands = getErrandsArea(req.body.areaID);
-	res.send({errands});
-    });
-    
+    /*app.post('getErrands', function(req, res) {
+	    var errands = getErrandsArea(req.body.areaID);
+	    res.send({errands});
+      });*/
+      
 });
 client.close();
