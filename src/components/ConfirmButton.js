272f2a66a98c66c65641812fc8d62318446705bb
Fabian Haglund <till.fabian@gmail.com>
2020-04-20 20:18:31 +0200

    rewrote ConfirmButton with FontAwesome

--- src/components/ConfirmButton.js
+++ src/components/ConfirmButton.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
 
-import { Checkmark } from 'react-checkmark';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 
 const ConfirmButton = (props) => {
     return (
-        <ConfirmButtonWrapper type="submit" value="Submit" id="confirm" onClick={props.onClickFunc}>
-            <Checkmark size='large'/>
+        <ConfirmButtonWrapper type="submit" value="Submit" id="confirm">
+            <FontAwesomeIcon icon={faCheckCircle} />
         </ConfirmButtonWrapper>
     );
 }
 
 const ConfirmButtonWrapper = styled.button`
-    background-color: #4CAF50;
-    border: none;
-    color: white;
+    background-color: white;
+    border: #4CAF50;
+    color: #4CAF50;
     padding: 0px;
     text-align: center;
     text-decoration: none;
     display: inline-block;
-    font-size: 16px;
-    margin: 4px 2px;
+    font-size: 48px;
     border-radius: 100%;
-
-
 `;
 
 export default ConfirmButton;
--- src/pages/ProfileCreation.js
+++ src/pages/ProfileCreation.js
@@ -52,40 +52,36 @@ class ProfileCreation extends React.Component {
 
     render(){
         return (
-                <div>
+            <div>
                 <NavBar />
-
+                
                 <form>
-                <SectionTitle text="Profile Creation" />
+                    <SectionTitle text="Profile Creation" />
 
-                <SectionTitle text="Username" />
-                <TextInput height="24px" name="username" value={this.username} saveInput={this.saveInput}/>
+                    <SectionTitle text="Username" />
+                    <TextInput height="24px" name="username" value={this.username} saveInput={this.saveInput}/>
 
-                <SectionTitle text="Password" />
-                <TextInput type="password" height="24px" name="password" value={this.password} saveInput={this.saveInput}/>
+                    <SectionTitle text="Password" />
+                    <TextInput type="password" height="24px" name="password" value={this.password} saveInput={this.saveInput}/>
 
-                <SectionTitle text="Given Name" />
-                <TextInput height="24px" name="firstname" value={this.firstname} saveInput={this.saveInput}/>
+                    <SectionTitle text="Given Name" />
+                    <TextInput height="24px" name="firstname" value={this.firstname} saveInput={this.saveInput}/>
 
-                <SectionTitle text="Surname" />
-                <TextInput height="24px" name="lastname" value={this.lastname} saveInput={this.saveInput}/>
+                    <SectionTitle text="Surname" />
+                    <TextInput height="24px" name="lastname" value={this.lastname} saveInput={this.saveInput}/>
 
-                <SectionTitle text="Age" />
-                <TextInput height="24px" name="age" type="number" value={this.age} saveInput={this.saveInput}/>
+                    <SectionTitle text="Age" />
+                    <TextInput height="24px" name="age" type="number" value={this.age} saveInput={this.saveInput}/>
 
-                <SectionTitle text="Mobile number" />
-                <TextInput height="24px" name="mobile" type="number" value={this.mobile} saveInput={this.saveInput}/>
+                    <SectionTitle text="Mobile number" />
+                    <TextInput height="24px" name="mobile" type="number" value={this.mobile} saveInput={this.saveInput}/>
 
-                <SectionTitle text="E-mail address" />
-                <TextInput height="24px" name="email" value={this.email} saveInput={this.saveInput}/>
+                    <SectionTitle text="E-mail address" />
+                    <TextInput height="24px" name="email" value={this.email} saveInput={this.saveInput}/>
 
-            {/*<Link to="residence-info">*/}
-                <ConfirmButton onClickFunc={this.storeSession} />
-                {/* </Link>*/}
-            </form>
-                </div>
-
-
+                    <ConfirmButton onClick={this.storeSession} />
+                </form>
+            </div>
         );
     }
 }
