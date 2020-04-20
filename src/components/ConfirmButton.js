7e810bddb84b7b8ca14a5602865afaf93689a651
Fabian Haglund <till.fabian@gmail.com>
2020-04-20 20:19:23 +0200

    ConfirmButton styling

--- src/components/ConfirmButton.js
+++ src/components/ConfirmButton.js
@@ -16,10 +16,7 @@ const ConfirmButtonWrapper = styled.button`
     background-color: white;
     border: #4CAF50;
     color: #4CAF50;
-    padding: 0px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
+    padding: 20px;
     font-size: 48px;
     border-radius: 100%;
 `;
