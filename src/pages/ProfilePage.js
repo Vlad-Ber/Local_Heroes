927fdbdb5d1e01064fe95e2a172d996c45d34e00
Benny <Benny.lam92@gmail.com>
2020-05-04 14:27:45 +0200

    Färdigställd, men funkar bara på Local Usercontext atm

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,21 +1,82 @@
-import React from 'react';
+import React, { Component } from "react";
+import NavBar from "../components/NavBar.js";
+import TextButton from "../components/TextButton.js";
+import LinkWrapper from "../components/LinkWrapper.js";
+import styled from "styled-components";
+import data from "../data/data.json";
+import EventItemListView from "../components/EventItemListView.js";
+import { render } from "@testing-library/react";
+import axios from "axios";
 
-import NavBar from '../components/NavBar.js';
-import TextButton from '../components/TextButton.js';
-import LinkWrapper from '../components/LinkWrapper.js';
+class ProfilePage extends Component {
+  constructor(props) {
+    super(props);
 
-const ProfilePage = (props) => {
+    this.state = {
+      user: this.props.activeUser.name,
+      address: this.props.activeUser.adress,
+      email: this.props.activeUser.email,
+      fetchErrandsSuccess: null,
+
+      errands: [],
+      profilePic: "https://image.flaticon.com/icons/png/512/37/37943.png",
+    };
+
+    this.getUserErrands = this.getUserErrands.bind(this);
+  }
+
+  getUserErrands = () => {
+    axios
+      .post("/getUserErrand", {
+        username: this.props.activeUser.username,
+      })
+      .then((response) => {
+        this.setState({
+          fetchErrandsSuccess: true,
+          errands: response.data["errands"],
+        });
+      })
+      .catch((error) => {
+        console.log("You have no errands!", error);
+        this.setState({
+          fetchErrandsSuccess: false,
+          errands: "You have no errands!",
+        });
+      });
+  };
+
+  componentDidMount() {
+    this.getUserErrands();
+  }
+
+  render() {
     return (
-        <div>
-            <NavBar
-                leftButtonType="back"
-                leftButtonLink="/home"
-            />
-            <LinkWrapper to="/">
-                <TextButton onClick={localStorage.clear()} description="LOG OUT"/>
-            </LinkWrapper>
-        </div>
+      <div>
+        <NavBar leftButtonType="back" leftButtonLink="/home" />
+        <StyledImage src={this.state.profilePic} />
+        <StyledText> Name: {this.state.user} </StyledText>
+        <StyledText>Area: {this.state.address} </StyledText>
+        <StyledText>E-mail: {this.state.email} </StyledText>
+        <StyledTextHeadLine>My Requests:</StyledTextHeadLine>
+
+        <EventItemListView errands={this.state.errands} />
+        <LinkWrapper to="/">
+          <TextButton onClick={localStorage.clear()} description="LOG OUT" />
+        </LinkWrapper>
+      </div>
     );
+  }
 }
+const StyledText = styled.h3`
+  margin: 0.5em;
+`;
+const StyledTextHeadLine = styled.h2`
+  margin: 0.5em;
+`;
+const StyledImage = styled.img`
+  margin: 2em;
+  width: 7em;
+  height: 7em;
+`;
 
 export default ProfilePage;
