179a2b6c90a2896eb37289cfd8b4a73e86526907
Jonathan <jonathannoahtadese@gmail.com>
2020-05-11 18:41:50 +0200

    made minor improvements to UI

--- src/components/TextButton.js
+++ src/components/TextButton.js
@@ -13,16 +13,29 @@ const TextButton = (props) => {
 const Button = styled.button`
     margin: auto;
     display: flex;
+
     justify-content: center;
     align-items: center;
     font-size: 1em;
+    font-family: 'Arial', Helvetica, sans-serif;
+
     height: ${props => props.height || '42px'};
     width: ${props => props.width || '18em'};
     margin-top: ${props => props.marginTop || '30px'};
     margin-bottom: ${props => props.marginBottom || '30px'};
+
     background: #31D285;
-    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-    font-family: 'Helvetica';
+    box-shadow: 1px;
+    border-radius: 4px;
+
+
+    &:hover {
+      cursor: pointer;
+
+      text-shadow: 0 0 2em rgba(250,250,250,1);
+      color:#FFFFFF;
+      border-color:#FFFFFF;
+    }
 `
 
 export default TextButton;
