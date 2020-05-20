48950227f32cf51e755e0d92559766fb94e103e6
Fabian Haglund <till.fabian@gmail.com>
2020-05-20 11:03:02 +0200

    added basename to router

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <UserProvider
           value={{
             ...this.state.userData,
