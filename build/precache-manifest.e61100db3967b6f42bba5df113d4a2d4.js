e4ea51e83f95eb1490e22e3e04b42b5b8319b2b5
Fabian Haglund <till.fabian@gmail.com>
2020-05-20 10:54:05 +0200

    build

---
+++ build/precache-manifest.e61100db3967b6f42bba5df113d4a2d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8f9517012faca3119d46ce57ef25bf",
+    "url": "/dsp-fess/index.html"
+  },
+  {
+    "revision": "8e0eb142178fef73d563",
+    "url": "/dsp-fess/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e5411ec3f9a23e5941a1",
+    "url": "/dsp-fess/static/js/2.a35e2d16.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dsp-fess/static/js/2.a35e2d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0eb142178fef73d563",
+    "url": "/dsp-fess/static/js/main.baee6dc3.chunk.js"
+  },
+  {
+    "revision": "7ce22c78280a0c5f322f",
+    "url": "/dsp-fess/static/js/runtime-main.4d867b34.js"
+  }
+]);
