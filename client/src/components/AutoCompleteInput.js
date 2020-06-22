8a2d48d31d1a77d0bb08eaa60f10805d68154d9c
Jonathan <jonathannoahtadese@gmail.com>
2020-06-22 17:07:52 +0200

    autocomplete works, have to integrate with rest of system

---
+++ client/src/components/AutoCompleteInput.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import styled from 'styled-components'
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
+
+
+class AutoCompleteInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props)
+
+  }
+
+  render() {
+    return (
+      <PlacesAutocomplete
+        onSelect={this.handleSelect}
+        {...this.props}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Address ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#a6a3a0', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+}
+
+const StyledInput = styled.input`
+    margin: auto;
+    display: flex;
+
+    font-family: 'Helvetica';
+    font-size: 14px;
+
+    border:  1px solid #31D285;
+    box-sizing: border-box;
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.12);
+
+    height: ${props => props.height || '24em'};
+    width: ${props => props.width || '18em'};
+`
+
+export default AutoCompleteInput;
--- client/src/views/ResidenceInfo.js
+++ client/src/views/ResidenceInfo.js
@@ -1,12 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 
 import NavBar from '../components/NavBar.js';
 import SectionTitle from '../components/SectionTitle.js';
 import TextInput from '../components/TextInput.js';
+import AutoCompleteInput from '../components/AutoCompleteInput.js';
 import ArrowButton from '../components/ArrowButton.js';
 import StyledForm from '../components/StyledForm.js';
 
-class ResidenceInfo extends Component {
+class ResidenceInfo extends React.Component {
 
       constructor(props){
         super(props);
@@ -21,6 +26,18 @@ class ResidenceInfo extends Component {
       saveInput = e => {
         this.setState({ [e.target.name]: e.target.value })
       }
+
+      setAdress = address => {
+        this.setState({ address: address })
+      }
+
+    /*  handleSelect = address => {
+        geocodeByAddress(address)
+          .then(results => getLatLng(results[0]))
+          .then(latLng => console.log('Success', latLng))
+          .catch(error => console.error('Error', error));
+      };*/
+
 
       storeSession = e => {
         window.sessionStorage.setItem("stateResidenceInfo", JSON.stringify(this.state));
@@ -40,11 +57,9 @@ class ResidenceInfo extends Component {
 
             <StyledForm>
               <SectionTitle text="Address" fontSize="14px" />
-              <TextInput
-              height="24px"
-              name="address"
-              value={this.address}
-              onChange={this.saveInput}
+              <AutoCompleteInput
+              value={this.state.address}
+              onChange={this.setAdress}
               />
 
               <SectionTitle text="Area Code" fontSize="14px"/>
